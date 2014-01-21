0ac81fa1b9d1bfa2e78d4d31feb2d6540490f3cc
Pierre <p.jolivet.pro@gmail.com>
2014-01-21 20:40:03 +0100

    Added extention options + recursive mode into templates folder

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-###Who to use **i18n_extract** python script for Polyglot ?###
+#Who to use ***i18n_extract.py*** python script for Polyglot ?
 
-	-First copy and paste the python script into your folder
-	-Then do a chmod +x on the script to make it executable (sudo chmod +x i18n_extract.py)
-	-Then execute the script without forgetting to set parameters
-		- "-t" is the directory where your templates file are stored
-		- "-l" is the directory where your translates file re stored
-	
-####Exemple###
+    -First copy and paste the python script into your folder
+    -Then execute the script without forgetting to set default settings
+        - "-t" is the directory where your templates file are stored
+        - "-l" is the directory where your translates file re stored
 
-	# ./i18n_extract.py -t < templates directory > -l < locales directory >
+    By default, these are files with the extension ".jade" that are sought. 
+    You can pass an optional parameter to specify the type of extension to be treated (.html, .hbs, ...)
+    To do this, write the same command with the "-e" option and the extension to be addressed preceded by a dot.
 
-####Result####
+        
+####Exemple:
+
+```python
+#For treated templates with default extention .jade
+./i18n_extract.py -t templates/ -l locales/
+
+#For treated templates with extention .html
+./i18n_extract.py -t templates/ -l locales/ -e ".html"
+```
+
+####Result:
 
     4 index were found in /i18n-extract/templates/sidebar.jade
     ['hello', 'sidebar', 'content', 'help']
@@ -20,7 +30,6 @@
 
     Processing /Library/WebServer/Documents/i18n-extract/locales/en.json file:
     ['Test'] as been deleted from the /i18n-extract/locales/en.json
-    ['Test2'] as been deleted from the file /i18n-extract/locales/en.json
     ['content'] as been added from the file /i18n-extract/locales/en.json
     ['sidebar'] as been added from the file /i18n-extract/locales/en.json
     ['hello'] as been added from the file /i18n-extract/locales/en.json
@@ -30,7 +39,6 @@
 
     Processing /Library/WebServer/Documents/i18n-extract/locales/fr.json file:
     ['Test'] as been deleted from the file /i18n-extract/locales/fr.json
-    ['Test2'] as been deleted from the file /i18n-extract/locales/fr.json
     ['content'] as been added from the file /i18n-extract/locales/fr.json
     ['sidebar'] as been added from the file /i18n-extract/locales/fr.json
     ['hello'] as been added from the file /i18n-extract/locales/fr.json
@@ -38,9 +46,16 @@
     ['help'] as been added from the file /i18n-extract/locales/fr.json
     Translation file is updated
 
-**OR, if the script is not executable**
+------------
+###Change Log
 
-	# python i18n_extract.py -t < templates directory > -l < locales directory >
+`0.2.0` - January 20, 2014
 
-####WARGING
-**The translated file must be json format and contain an valid json object**
+-Adding recurrence to allow fully search the directory templates and records are in
+
+-Added ability to set option (**-e**), to special extention
+
+
+`0.1.0` - January 19, 2014
+
+-Initial i18n_extract release.  Added source code and documentation.
--- i18n_extract.py
+++ i18n_extract.py
@@ -9,30 +9,49 @@ import stat
 import re
 import pprint
 import json
-
+import fnmatch
 
 class I18nExtract:
     regex = r"polyglot.t\(['\"](.*)['\"]\)"
     __correct_index = []
+    __file_extention = "*.jade"
 
-    def __init__(self, template_folder, locale_folder):
+    def __init__(self, template_folder, locale_folder, file_extention):
         self.template_folder = template_folder
         self.locale_folder = locale_folder
+        
+        if file_extention is not None:
+            self.__file_extention = "*"+file_extention
+
         self.__extract_template_keys()
 
     def __extract_template_keys(self):
-        #print os.path.realpath(self.locale_folder)
-        for template_file in glob.glob(os.path.join(os.path.realpath(self.template_folder), "*.jade")):
-            file_ = open(template_file).read()
-            results = re.findall(self.regex, file_)
-            print "%s index were found in %s" % (len(results), template_file)
-            pprint.pprint(results)
+        
+        '''
+        Finds all files in the given directory, as well as those in subdirectories
+        '''
+        template = [
+            os.path.join(dirpath, f)
+            for dirpath, dirnames, files in os.walk(os.path.join(os.path.realpath(self.template_folder)))
+            for f in fnmatch.filter(files, self.__file_extention)
+        ]
+
+        '''
+        Just a simple loop for read and add current key to a list
+        '''
+        for file_ in template:
+            results = re.findall(self.regex, open(file_).read())
+            print "%s index were found in %s: %s\n" % (len(results), file_, results)
+            #pprint.pprint(results, width=150)
 
             for result in results:
                 self.__correct_index.append(result)
 
         self.__update_locales()
 
+    '''
+    Intert the news keys founded in all templates
+    '''
     def __insert_data(self, json_file):
         for data_index in set(self.__correct_index):
             if data_index not in self.json_datas.keys():
@@ -41,6 +60,9 @@ class I18nExtract:
 
         json.dump(self.json_datas, open(json_file, 'w'), indent=4, sort_keys=True)
 
+    '''
+    Updates all the keys contained in the nTranslation files
+    '''
     def __remove_data(self, json_file):
         for key in self.json_datas.keys():
             if key not in set(self.__correct_index):
@@ -49,34 +71,42 @@ class I18nExtract:
 
         json.dump(self.json_datas, open(json_file, 'w'), indent=4, sort_keys=True)
 
+    '''
+    Call __remove_data function and __insert_data function
+    '''
     def __update_locales(self):
         for json_file in glob.glob(os.path.join(os.path.realpath(self.locale_folder), "*.json")):
             if os.stat(json_file)[stat.ST_SIZE] > 0:
-                print "\nProcessing %s file:" % json_file
+                print "\nPROCESSING %s file:" % json_file
                 with open(json_file, "r") as feed_json:
                     try:
                         self.json_datas = json.loads(feed_json.read())
 
                         self.__remove_data(json_file)
                         self.__insert_data(json_file)
-                        print "\nTranslation file is updated"
+                        print "\nTRANSLATION FILE IS UPDATED"
                     except (ValueError, KeyError, TypeError):
                         print "\n--------------- JSON FORMAT ERROR ---------------\n ValuError: %s\n KeyError: %s\n TypeError: %s\n" % (
                             ValueError, KeyError, TypeError)
 
 
+
 try:
     if len(sys.argv) > 1:
+        options, args = getopt.getopt(sys.argv[1:], "t:l:e:")
         templates_folder = None
         locales_folder = None
-        options, args = getopt.getopt(sys.argv[1:], "t:l:")
+        file_extention = None
+        
         for o, a in options:
             if o == '-t':
                 templates_folder = a
             elif o == '-l':
                 locales_folder = a
+            elif o == '-e':
+                file_extention = a
         if templates_folder is not None and locales_folder is not None:
-            I18nExtract(templates_folder, locales_folder)
+            I18nExtract(templates_folder, locales_folder, file_extention)
     else:
         print("Usage: %s -t < templates folder > -l < locales folder >" % sys.argv[0])
 except:
